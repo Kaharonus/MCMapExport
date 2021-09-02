dd2bfdaed33932473bdbce66dc0dd74d61b1deb0
Tomáš Oplatek <tomas.oplatek@gmail.com>
2021-09-02 17:55:53 +0200

    updated fragment shader

--- MCMapExport.MapRenderer/Shaders/fragment.glsl
+++ MCMapExport.MapRenderer/Shaders/fragment.glsl
@@ -11,7 +11,9 @@ uniform sampler2D tex;
   
 void main(){
     //FragColor = texture(tex, texcoords);
-    FragColor = mix(texture(tex, texcoords), vec4(0.5,0.5,0.5,1), 0.2);
+    vec4 texColor = texture(tex, texcoords);
+    //FragColor = mix(vec4(0.2), texColor, texColor.a);
+    FragColor = mix(texColor, vec4(0.5,0.5,0.5,1), 0.2);
     //FragColor = mix(vec4(texcoords.x, texcoords.y, 0 , 1), vec4(0.5,0.5,0.5,1), 0.2);
     //FragColor = vec4(1,1,1,1);
 } 
